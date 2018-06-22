18ce1a1c10a5e5ce404cc541a2ac5f773609d4a0
David Becker <david.becker.online@freenet.de>
2018-06-22 13:40:09 +0200

    mal wider zu dum für git...

---
+++ Testat/Assets/Schader/CameraController2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController2 : MonoBehaviour
+{
+    
+
+    private Vector3 offset;
+    public float intensity;
+    public Material material;
+
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/Sepia"));
+    }
+
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (intensity == 0)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+        material.SetFloat("_sepiaBlend", intensity);
+        Graphics.Blit(source, destination, material);
+    }
+}
