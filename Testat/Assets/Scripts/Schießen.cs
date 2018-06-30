64763f3b754e52914c125a6e5e7caced0ab9e2f8
lukas <pockrandt96@gmail.com>
2018-06-30 22:29:38 +0200

    Added final Version of deformation

--- Testat/Assets/Scripts/Schießen.cs
+++ Testat/Assets/Scripts/Schießen.cs
@@ -12,16 +12,29 @@ public class Schießen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		
-
-		if (Input.GetMouseButtonDown(0))
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        int numberOfBalls = 0;
+        foreach (GameObject go in allObjects)
+            if (go.activeInHierarchy)
+            {
+                if (go.name.Equals("ball"))
+                {
+                    numberOfBalls++;
+                    if (numberOfBalls > 100)
+                    {
+                        Destroy(go);
+                        numberOfBalls--;
+                    }
+                }
+            }
+               
+        if (Input.GetMouseButtonDown(0))
 		{
 			GameObject projectile = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			projectile.name = "ball";
 			projectile.transform.position = gameObject.transform.position + Camera.main.transform.forward * 2;
 			projectile.AddComponent<Rigidbody>();
-			projectile.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * 50;
+            projectile.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * 50;
 		}
 	}
 }
